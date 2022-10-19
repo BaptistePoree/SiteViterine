2d5b3da3aa6d8ca8ddf5454d71f775d66ac336af
Poree Baptiste <baptistePoree@gmail.com>
2022-10-19 15:32:55 +0200

    mise a jour

--- SRC/templates/general/top.php
+++ SRC/templates/general/top.php
@@ -14,32 +14,4 @@
     <?php endif ?>
 </head>
 
-<style>
-    /* .closed {
-        display: none;
-    }
-
-    .bg-modal {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.7);
-        margin-top: -30px;
-    }
-
-    .popup-modal {
-        max-width: 500px;
-        max-height: 300px;
-        background: white;
-        border-radius: 10px;
-        margin: auto auto;
-        padding: 20px;
-    }
-
-    body {
-        margin: 0;
-    } */
-</style>
-
-
 <body>
